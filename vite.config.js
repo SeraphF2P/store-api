87179ab17aa03254b7500f550927922f049dfad4
SeraphF2P <110803319+SeraphF2P@users.noreply.github.com>
2023-05-22 16:44:33 +0300

    fixed a bug

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,7 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
   plugins: [
     laravel({
-      input: ['resources/ts/react/App.css',
-        'resources/ts/react/main.tsx'],
+      input: ['resources/css/app.css', 'resources/js/app.js'],
       refresh: true,
     }),
   ],
